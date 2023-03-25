d4693d3de0ecbcc98fdae763374d0452e2b075c2
DenisTihonkih <denist2002@gmail.com>
2023-03-25 17:43:49 +0900

    Готова главная страница, но не подключены шрифты

---
+++ js/script.js
@@ -0,0 +1,61 @@
+const swiper = new Swiper('.swiper', {
+   loop: true,
+   navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+   },
+   spaceBetween: 32,
+   autoplay: {
+      delay: 6000,
+      disableOnInteraction: false
+   },
+   speed: 500,
+});
+
+let title = () => {
+   let title = document.querySelector('title'),
+      menuItemActive = document.querySelector('.header__menu > .header__menu-item > ._active');
+      title.textContent = menuItemActive.textContent
+}
+title();
+
+let burger = () => {
+   const menuBurger = document.querySelector('.header__burger');
+   const footerMenuBurger = document.querySelector('.footer__burger');
+
+   const footerMenu = document.querySelector('.footer__menu');
+   const menu = document.querySelector('.header__menu');
+
+   const headerMenuItem = document.querySelectorAll('.header__menu-item > a');
+   const footerMenuItem = document.querySelectorAll('.footer__menu-item > a');
+
+   const arrayColor = [];
+
+   headerMenuItem.forEach((item) => {
+      arrayColor.push(item);
+   })
+   footerMenuItem.forEach((item) => {
+      arrayColor.push(item);
+   })
+
+   const body = document.querySelector('body');
+   menuBurger.addEventListener('click', function (e) {
+      menuBurger.classList.toggle('_active');
+      menu.classList.toggle('_active');
+      body.classList.toggle('_lock');
+      arrayColor.forEach((item) => {
+         item.classList.toggle('_active-color');
+      })
+   })
+
+   footerMenuBurger.addEventListener('click', () => {
+      footerMenu.classList.toggle('_active');
+      footerMenuBurger.classList.toggle('_active');
+      body.classList.toggle('_lock');
+      arrayColor.forEach((item) => {
+         item.classList.toggle('_active-color');
+      })
+   })
+}
+burger();
+
