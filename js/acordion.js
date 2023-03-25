d896f2bf5078f9f35cc3b0a7b494b20e63b20dd0
DenisTihonkih <denist2002@gmail.com>
2023-03-25 23:23:22 +0900

    Сделал аккордион

--- js/acordion.js
+++ js/acordion.js
@@ -1,15 +1,19 @@
-let acordion = () => {
-   let labelItem = document.querySelectorAll('._label__item');
-
-   labelItem.forEach(item => {
-      let labelBtn = item.querySelector('._label__title');
-      let labelContent = item.querySelector('._label__description');
-
-      labelBtn.addEventListener('click', () => {
-         labelContent.classList.add('open')
-
-         labelContent.style.height = labelBtn.scrollHeight + 'px';
+let accordeon = () => {
+   const chItems = document.querySelectorAll('._label__item');
+   chItems.forEach(item => {
+      const chButton = item.querySelector('._label__title');
+      const chContent = item.querySelector('._label__description');
+      
+      chButton.addEventListener('click', () => {
+         chItems.forEach(item => {
+            const chButton2 = item.querySelector('._label__title');
+            const chContent2 = item.querySelector('._label__description');
+            chButton2.classList.remove('active');
+            chContent2.style.height = '';
+         })
+         chButton.classList.add('active');
+         chContent.style.height = chContent.scrollHeight + 'px';
       })
    })
 }
-acordion();
+accordeon();
